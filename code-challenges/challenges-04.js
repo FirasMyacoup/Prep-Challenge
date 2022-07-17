781366821bf3dd9bf000694a5f99e0bb0905b643
FirasMyacoup <firas.yacoup1@gmail.com>
2022-07-17 14:00:18 +0300

    exercise04 and 03

--- code-challenges/challenges-04.js
+++ code-challenges/challenges-04.js
@@ -9,13 +9,18 @@
 // Input: nums = [1,2,3,4]
 // Output: [1,3,6,10]
 // Note that Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
-const runningSumOfArray = function(num) {
-  let runningSumArray= new Arr(num.length);
-  runningSumArray[0] = num[0];
-  for (let i = 1; i < num.length; i++);
-  runningSumArray[i] = runningSumArray[i-1] + nums[i];
-  return runningSumArray;
+const runningSumOfArray = nums=> {
+  let results= [];
+  let sum=0
+  for (let i = 0; i < nums.length; i++){
+ let val=nums[i];
+ sum += val;
+  results.push(sum);
+}
+return results;
 };
+//console.log[runningSumOfArray([1,2,3,4])];//[1,3,6,10]
+
 // -------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------
